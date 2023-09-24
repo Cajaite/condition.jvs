dadeec1664a0da92adf68e2e3da7ab1c0a25c36e
Cajaite <145431034+Cajaite@users.noreply.github.com>
2023-09-24 22:00:36 +0200

    Add files via upload

---
+++ CONDITION.js
@@ -0,0 +1,28 @@
+"use strict";
+
+/**           CONDITION
+ * declarer trois variable l=6,m=6,n=5
+ * 
+ * l sera l expression de reference 
+ * si l est egal a m alors faire un console log('egal à m')
+ * 
+ * si l est egal a n alors faire un console log('egal à m')
+ * 
+ * par defaut il y a un console log("egal a rien")
+ */
+
+
+let l=6;
+let m=6;
+let n=5;
+
+switch(a){
+    case b:
+        console.log("égal à b");
+        break;
+    case c:
+        console.log("égal à c");
+        break;
+    default:
+        console.log("égal à rien");
+}
